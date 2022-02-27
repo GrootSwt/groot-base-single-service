dc0a9c607d0fc2623477ec839c3c26a5cc1847d4
swt <396518038@qq.com>
2022-02-27 13:52:16 +0800

    备份数据库

--- src/main/resources/single_blog.sql
+++ src/main/resources/single_blog.sql
@@ -1,28 +1,129 @@
-/*
- Navicat Premium Data Transfer
-
- Source Server         : aliyun
- Source Server Type    : MySQL
- Source Server Version : 80027
- Source Host           : 39.106.49.0:3306
- Source Schema         : single_blog
-
- Target Server Type    : MySQL
- Target Server Version : 80027
- File Encoding         : 65001
-
- Date: 20/01/2022 16:35:50
-*/
-
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for audit_log
+-- ----------------------------
+DROP TABLE IF EXISTS `audit_log`;
+CREATE TABLE `audit_log`  (
+  `id` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `create_time` datetime NULL DEFAULT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `login_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `phone_number` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `success` bit(1) NULL DEFAULT NULL,
+  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `full_class_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `method_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `request_data` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
+  `response_data` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
+  `simple_class_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `request_method` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `service_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `request_uri` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `resolved` bit(1) NULL DEFAULT NULL,
+  `ip` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 3265 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of audit_log
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for audit_log_history
+-- ----------------------------
+DROP TABLE IF EXISTS `audit_log_history`;
+CREATE TABLE `audit_log_history`  (
+  `id` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `create_time` datetime NULL DEFAULT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `full_class_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `login_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `method_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `phone_number` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `request_data` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
+  `request_method` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `request_uri` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `resolved` bit(1) NULL DEFAULT NULL,
+  `response_data` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
+  `service_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `simple_class_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `success` bit(1) NULL DEFAULT NULL,
+  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `create_time`(`create_time`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 2659 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of audit_log_history
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for dictionary
+-- ----------------------------
+DROP TABLE IF EXISTS `dictionary`;
+CREATE TABLE `dictionary`  (
+  `id` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `category_id` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `create_time` datetime NULL DEFAULT NULL,
+  `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `dictionary_key` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `dictionary_value` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `enabled` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `modify_time` datetime NULL DEFAULT NULL,
+  `parent_id` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 6899268307782733825 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of dictionary
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for dictionary_category
+-- ----------------------------
+DROP TABLE IF EXISTS `dictionary_category`;
+CREATE TABLE `dictionary_category`  (
+  `id` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `category_key` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `category_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `create_time` datetime NULL DEFAULT NULL,
+  `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `enabled` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `modify_time` datetime NULL DEFAULT NULL,
+  `service_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of dictionary_category
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for file_info
+-- ----------------------------
+DROP TABLE IF EXISTS `file_info`;
+CREATE TABLE `file_info`  (
+  `id` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `file_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `file_path` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `file_size` bigint NULL DEFAULT NULL,
+  `file_type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `files_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 58 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of file_info
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for menu
 -- ----------------------------
 DROP TABLE IF EXISTS `menu`;
 CREATE TABLE `menu`  (
-  `id` bigint NOT NULL AUTO_INCREMENT,
+  `id` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `title` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '标题',
   `location` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '路径',
   `icon` varchar(40) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '图标',
@@ -31,85 +132,89 @@ CREATE TABLE `menu`  (
   `parent_id` bigint NOT NULL COMMENT '父菜单ID',
   `type` varchar(1) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '类型（1：菜单；2：权限）',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 21 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 24 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of menu
 -- ----------------------------
-INSERT INTO `menu` VALUES (1, '主页', '/home', 'el-icon-s-home', 1, '1', 0, '1');
-INSERT INTO `menu` VALUES (2, '系统管理', '/system', 'el-icon-s-tools', 2, '1', 0, '1');
-INSERT INTO `menu` VALUES (3, '权限管理', '/system/menu', 'el-icon-menu', 1, '1', 2, '1');
-INSERT INTO `menu` VALUES (11, '角色管理', '/system/role', 'fa fa-chain', 2, '1', 2, '1');
-INSERT INTO `menu` VALUES (12, '用户管理', '/system/user', 'el-icon-user-solid', 3, '1', 2, '1');
-INSERT INTO `menu` VALUES (18, '日志管理', '/system/log', 'fa fa-bug', 4, '1', 2, '1');
-INSERT INTO `menu` VALUES (19, '数据字典管理', '/system/dict', 'fa fa-stack-overflow', 5, '1', 2, '1');
-INSERT INTO `menu` VALUES (20, 'JAVA', '/java', 'el-icon-hot-water', 3, '1', 0, '1');
-INSERT INTO `menu` VALUES (21, '新增同级节点', 'addSameNode', NULL, 1, '1', 3, '2');
-INSERT INTO `menu` VALUES (22, '新增子节点', 'addSubNode', NULL, 2, '1', 3, '2');
-INSERT INTO `menu` VALUES (23, '删除该节点', 'deleteNode', NULL, 3, '1', 3, '2');
+INSERT INTO `menu` VALUES ('1', '主页', '/home', 'el-icon-s-home', 1, '1', 0, '1');
+INSERT INTO `menu` VALUES ('11', '角色管理', '/system/role', 'fa fa-chain', 2, '1', 2, '1');
+INSERT INTO `menu` VALUES ('12', '用户管理', '/system/user', 'el-icon-user-solid', 3, '1', 2, '1');
+INSERT INTO `menu` VALUES ('18', '日志管理', '/system/log', 'fa fa-bug', 4, '1', 2, '1');
+INSERT INTO `menu` VALUES ('19', '数据字典管理', '/system/dict', 'fa fa-stack-overflow', 5, '1', 2, '1');
+INSERT INTO `menu` VALUES ('2', '系统管理', '/system', 'el-icon-s-tools', 2, '1', 0, '1');
+INSERT INTO `menu` VALUES ('20', 'JAVA', '/java', 'el-icon-hot-water', 4, '1', 0, '1');
+INSERT INTO `menu` VALUES ('21', '新增同级节点', 'addSameNode', NULL, 1, '1', 3, '2');
+INSERT INTO `menu` VALUES ('22', '新增子节点', 'addSubNode', NULL, 2, '1', 3, '2');
+INSERT INTO `menu` VALUES ('23', '删除该节点', 'deleteNode', NULL, 3, '1', 3, '2');
+INSERT INTO `menu` VALUES ('3', '权限管理', '/system/menu', 'el-icon-menu', 1, '1', 2, '1');
 
 -- ----------------------------
 -- Table structure for role
 -- ----------------------------
 DROP TABLE IF EXISTS `role`;
 CREATE TABLE `role`  (
-  `id` bigint NOT NULL AUTO_INCREMENT,
+  `id` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `name` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '角色名',
   `description` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '角色描述',
   `enabled` varchar(1) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '是否启用',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 13 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 15 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of role
 -- ----------------------------
-INSERT INTO `role` VALUES (13, '管理员', '具有系统管理权限', '1');
+INSERT INTO `role` VALUES ('13', '管理员', '具有系统管理权限', '1');
+INSERT INTO `role` VALUES ('14', '普通用户', '不具有系统管理用户', '1');
 
 -- ----------------------------
 -- Table structure for role_relation_menu
 -- ----------------------------
 DROP TABLE IF EXISTS `role_relation_menu`;
 CREATE TABLE `role_relation_menu`  (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `role_id` bigint NOT NULL COMMENT '角色ID',
-  `menu_id` bigint NOT NULL COMMENT '菜单ID',
+  `id` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `role_id` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '角色ID',
+  `menu_id` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '菜单ID',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 168 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 180 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of role_relation_menu
 -- ----------------------------
-INSERT INTO `role_relation_menu` VALUES (168, 13, 1);
-INSERT INTO `role_relation_menu` VALUES (169, 13, 2);
-INSERT INTO `role_relation_menu` VALUES (170, 13, 3);
-INSERT INTO `role_relation_menu` VALUES (171, 13, 21);
-INSERT INTO `role_relation_menu` VALUES (172, 13, 22);
-INSERT INTO `role_relation_menu` VALUES (173, 13, 23);
-INSERT INTO `role_relation_menu` VALUES (174, 13, 11);
-INSERT INTO `role_relation_menu` VALUES (175, 13, 12);
-INSERT INTO `role_relation_menu` VALUES (176, 13, 18);
-INSERT INTO `role_relation_menu` VALUES (177, 13, 19);
+INSERT INTO `role_relation_menu` VALUES ('168', '13', '1');
+INSERT INTO `role_relation_menu` VALUES ('169', '13', '2');
+INSERT INTO `role_relation_menu` VALUES ('170', '13', '3');
+INSERT INTO `role_relation_menu` VALUES ('171', '13', '21');
+INSERT INTO `role_relation_menu` VALUES ('172', '13', '22');
+INSERT INTO `role_relation_menu` VALUES ('173', '13', '23');
+INSERT INTO `role_relation_menu` VALUES ('174', '13', '11');
+INSERT INTO `role_relation_menu` VALUES ('175', '13', '12');
+INSERT INTO `role_relation_menu` VALUES ('176', '13', '18');
+INSERT INTO `role_relation_menu` VALUES ('177', '13', '19');
+INSERT INTO `role_relation_menu` VALUES ('178', '14', '1');
+INSERT INTO `role_relation_menu` VALUES ('179', '14', '20');
 
 -- ----------------------------
 -- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user`  (
-  `id` bigint NOT NULL AUTO_INCREMENT,
+  `id` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `username` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户名',
-  `password` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '密码',
+  `password` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '密码',
   `login_name` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '登录名',
   `phone_number` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '联系方式',
   `email` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '邮箱',
-  `role_id` bigint NOT NULL COMMENT '角色ID',
+  `role_id` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '角色ID',
   `enabled` varchar(1) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '启用状态（0：未启用；1：启用）',
-  `avatar` bigint NULL DEFAULT NULL,
+  `avatar` bigint NULL DEFAULT NULL COMMENT '头像',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES (1, 'admin', '123456', 'admin', '18888888888', '18888888888@micro.com', 13, '1', 55);
+INSERT INTO `user` VALUES ('1', 'admin', '6d3de71a35ad508b729ba1f888265746', 'admin', '17777777777', '17777777777@micro.com', '13', '1', 55);
+INSERT INTO `user` VALUES ('6', 'swt', '6d3de71a35ad508b729ba1f888265746', 'swt', '18888888888', '18888888888@qq.com', '14', '1', 57);
 
 SET FOREIGN_KEY_CHECKS = 1;
